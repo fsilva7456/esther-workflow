555375d5143c2ccfdba99131d565215cb8945a4c
Francis Silva <f.silva@interceptgroup.com>
2025-11-22 13:40:55 -0500

    Fix: Remove duplicated code in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,27 +8,17 @@ import { TestDashboard } from './screens/TestDashboard';
 
 function App() {
     return (
-import React from 'react';
-    import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-    import { ProjectSelector } from './screens/ProjectSelector';
-    import { UseCaseEditor } from './screens/UseCaseEditor';
-    import { TestGenerator } from './screens/TestGenerator';
-    import { AgentHandoff } from './screens/AgentHandoff';
-    import { TestDashboard } from './screens/TestDashboard';
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/projects" replace />} />
+                <Route path="/projects" element={<ProjectSelector />} />
+                <Route path="/projects/:projectId/use-cases" element={<UseCaseEditor />} />
+                <Route path="/projects/:projectId/use-cases/:useCaseId/tests" element={<TestGenerator />} />
+                <Route path="/projects/:projectId/use-cases/:useCaseId/agent" element={<AgentHandoff />} />
+                <Route path="/projects/:projectId/tests" element={<TestDashboard />} />
+            </Routes>
+        </Router>
+    );
+}
 
-    function App() {
-        return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Navigate to="/projects" replace />} />
-                    <Route path="/projects" element={<ProjectSelector />} />
-                    <Route path="/projects/:projectId/use-cases" element={<UseCaseEditor />} />
-                    <Route path="/projects/:projectId/use-cases/:useCaseId/tests" element={<TestGenerator />} />
-                    <Route path="/projects/:projectId/use-cases/:useCaseId/agent" element={<AgentHandoff />} />
-                    <Route path="/projects/:projectId/tests" element={<TestDashboard />} />
-                </Routes>
-            </Router>
-        );
-    }
-
-    export default App;
+export default App;
